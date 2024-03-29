e9efef38fff342959776507f51bc155c0a210979
Sldark23 <115877460+Sldark23@users.noreply.github.com>
2024-03-29 10:48:08 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+// Configurações do banco de dados
+$host = 'localhost'; // Host do PostgreSQL (normalmente 'localhost' para ambiente local)
+$dbname = 'prtube_db'; // Nome do banco de dados
+$user = 'host'; // Usuário do banco de dados
+$password = 'host'; // Senha do banco de dados
