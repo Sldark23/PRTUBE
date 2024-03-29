82a49c124c4d2c0ef74cbd08d4c84b233af2beb1
Sldark23 <115877460+Sldark23@users.noreply.github.com>
2024-03-29 10:56:40 -0300

    Update config.php

--- config.php
+++ config.php
@@ -4,3 +4,10 @@ $host = 'localhost'; // Host do PostgreSQL (normalmente 'localhost' para ambient
 $dbname = 'datebase'; // Nome do banco de dados
 $user = 'host'; // Usuário do banco de dados
 $password = 'host'; // Senha do banco de dados
+try {
+    $conn = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo 'Erro ao conectar ao banco de dados: ' . $e->getMessage();
+}
+?>
